13564a04cded325d93552bc399f1e6609fc8ac0a
Hariadi Adha F <hariadi.adha@gmail.com>
2019-03-28 18:33:53 +0700

    update navbar - search button

--- penzu_ci/application/views/Jurnal.php
+++ penzu_ci/application/views/Jurnal.php
@@ -15,14 +15,20 @@
 
 </head>
 <body>
-  <div class="d-flex justify-content-left">
-    <nav class="navbar navbar-light bg-light ">
-      <form class="form-inline align-baseline">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </nav>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
   </div>
+</nav>
 
 
 </body>
