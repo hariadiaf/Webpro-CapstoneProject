06a7f9d7d43849d5428cfd50f8f40432fda01229
bianis <febiana.anistya@gmail.com>
2019-03-28 16:26:57 +0700

    6th commit

--- penzu_ci/application/views/welcome_message.php
+++ penzu_ci/application/views/welcome_message.php
@@ -3,9 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 	<meta charset="utf-8">
 	<title>Write In Private: Free Online Diary And Personal Journal | Penzu</title>
 </head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <body>
 <div class= container>
 <img src="assets/img/pz-logo__red.png" alt="">
