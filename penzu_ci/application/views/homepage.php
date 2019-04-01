24f05fc0e6ce4b7a20c66761d8942dea09ac8e4d
bianis <febiana.anistya@gmail.com>
2019-04-01 22:38:56 +0700

    homepage change

---
+++ penzu_ci/application/views/homepage.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets\homepage.css">    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<title>Write In Private: Free Online Diary And Personal Journal | Penzu</title>
+</head> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<body>
+    <div class="firstlog">
+        <a class="btn" href="/view/login" target="_self">Log in</a>
+        <a class="btn" href="" ng-click="$ctrl.openSignupModal()">Sign up</a>    
+    </div>
+    <div class="part1" style='background-image : url("assets/img/background-desk.jpg") !important;
+      background-position: center;
+    background-repeat : no-repeat;
+    background-size: cover;
+    height : 846.594px;
+    width : 1349px;
+    display : block;
+    overflow : hidden;'>
+        <div class="container logoplus">
+            <center>
+                <img id="logomain" src="assets/img/pz-logo__red.png" alt=""> <br>   
+                Your private, 100% customizable online journal.<br>
+                Loved by over 2 million writers around the world. <br><br><br>
+                <a class="btn btn-block btn-primary" id="btnsignup" href="" style="">Start Your Free Journal Now!</a>
+            </center>
+        </div>
+    </div>
+    <div class="container sponsored">
+            <p>Loved by more than 2,000,000 writers, featured on...</p>
+        </div>
+    
+</body>
+
+</html>
