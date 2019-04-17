a8cdb20116515fc26fbb9cb1b4d57a0843cebd0d
Hariadi Adha F <hariadi.adha@gmail.com>
2019-04-17 18:54:35 +0700

    au ah gelap

--- penzu_ci/application/views/Jurnal.php
+++ penzu_ci/application/views/Jurnal.php
@@ -14,60 +14,68 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
 </head>
+
 <body>
+  <style media="screen">
+    body {
+      background-image: url("assets/img/wood.jpg");
+    }
+  </style>
+
+<div class="sticky-top">
   <nav class="navbar navbar-expand-lg navbar-light" style="background-color : #EF4228; border-radius : 0px;">
-  <!-- <a class="navbar-brand" href="#">Navbar</a> -->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
 
-    <form class="form-inline ">
-      <div class="input-group flex-nowrap" style="display: inherit; background-color : #B00B1B; border-radius : 20px;">
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="addon-wrapping"
-          style="border: none; border-top-left-radius: 20px; border-bottom-left-radius: 20px; background-color : #B00B1B;"> <img src="<?= base_url('assets/img/search.png')?>" width="18px" > </span>
+      <form class="form-inline ">
+        <div class="input-group flex-nowrap" style="display: inherit; background-color : #B00B1B; border-radius : 20px;">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="addon-wrapping"
+            style="border: none; border-top-left-radius: 20px; border-bottom-left-radius: 20px; background-color : #B00B1B;"> <img src="<?= base_url('assets/img/search.png')?>" width="18px" > </span>
+          </div>
+          <input type="text" class="form-control" placeholder="Search" aria-label="Username" aria-describedby="addon-wrapping"
+          style="border: none; border-radius : 20px; background-color : #B00B1B;">
         </div>
-        <input type="text" class="form-control" placeholder="Search" aria-label="Username" aria-describedby="addon-wrapping"
-        style="border: none; border-radius : 20px; background-color : #B00B1B;">
-      </div>
-      <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search"
-      style="border: none;border-radius : 20px; background-color : #B00B1B;
-      background : url(../assets/img/search.png) no-repeat scroll 7px 7px;
-      padding-left : 30px"> -->
+        <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search"
+        style="border: none;border-radius : 20px; background-color : #B00B1B;
+        background : url(../assets/img/search.png) no-repeat scroll 7px 7px;
+        padding-left : 30px"> -->
 
-      <!-- <button class="btn btn-outline-success my-2 my-sm-0 Rounded corners" type="submit">Search</button> -->
-      <!-- <img src=".../assets/img/penzu_white.png" alt=""> -->
-      <!-- test -->
-      <div class="text-center" style="margin-left : 340px; ">
-        <img style="width : 100px;" src="<?= base_url('assets/img/penzu_white.png')?>" width="10px">
-      </div>
-      <div class="float-right" style="margin-left : 340px;">
-        <a href="#" name="btnGoPro" style="background-color : #ffffff; padding : 6px 8px; text-decoration: none;  font-weight : bold; border-radius : 6px; font-style : bold; color : #E74C3C;">Go PRO</a>
-      </div>
-
-      <style>
-      .vl {
-        border-left: 6px solid black;
-        height: 500px;
-      }
-      </style>
-
-      <div class="dropdown">
-        <button class="btn  dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="border : none; color : #FFFFFF;">
-          User
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-          <button class="dropdown-item" type="button">Action</button>
-          <button class="dropdown-item" type="button">Another action</button>
-          <button class="dropdown-item" type="button">Something else here</button>
+        <!-- <button class="btn btn-outline-success my-2 my-sm-0 Rounded corners" type="submit">Search</button> -->
+        <!-- <img src=".../assets/img/penzu_white.png" alt=""> -->
+        <!-- test -->
+        <div class="text-center" style="margin-left : 340px; ">
+          <img style="width : 100px;" src="<?= base_url('assets/img/penzu_white.png')?>" width="10px">
         </div>
-      </div>
-    </form>
-  </div>
-</nav>
-<div class="" style="background-color : #2F3940; position: relative; bottom: 24px; padding : 12px;">
+        <div class="float-right" style="margin-left : 340px;">
+          <a href="#" name="btnGoPro" style="background-color : #ffffff; padding : 6px 8px; text-decoration: none;  font-weight : bold; border-radius : 6px; font-style : bold; color : #E74C3C;">Go PRO</a>
+        </div>
+
+        <style>
+        .vl {
+          border-left: 6px solid black;
+          height: 500px;
+        }
+        </style>
+
+        <div class="dropdown">
+          <button class="btn  dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="border : none; color : #FFFFFF;">
+            User
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+            <button class="dropdown-item" type="button">Action</button>
+            <button class="dropdown-item" type="button">Another action</button>
+            <button class="dropdown-item" type="button">Something else here</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </nav>
+  <div class="" style="background-color : #2F3940; position: relative; bottom: 24px; padding : 12px;">
     <a href="#" class="float-right" name="btnNewJournal" style="background-color : #258CD1; padding : 6px 8px;
     text-decoration: none;  font-weight : bold; border-radius : 6px; font-style : bold; color : #ffffff; margin-right : 180px;"> <img src="<?= base_url('assets/img/book.png');?>" width="20px"> New Journal</a>
 
@@ -78,9 +86,24 @@
     text-decoration: none;  font-weight : bold; border-radius : 6px; font-style : bold; color : #ffffff; margin-right : 8px;"> <img src="<?= base_url('assets/img/brush.png') ?>" width="20px"> Change Themes</a>
 
     <h6 style="margin-left : 200px; font-size: 16px; color : #FFFFFF;" >Displaying 1 Journal</h6>
-
+  </div>
 </div>
 
+
+
+<div class="" style="margin-left : 100px;">
+  <img src="<?= base_url('assets/img/Capture.PNG') ?>" width="680px">
+  <div class="">
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+  </div>
+</div>
 
 </body>
 </html>
